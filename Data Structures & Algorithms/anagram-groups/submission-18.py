1157e238aed666683d0c16297d0f1959531ad78f
Drayiss <139669637+Drayiss@users.noreply.github.com>
2026-09-16 07:07:17 -0400

    Add: anagram-groups - submission-18

---
+++ Data Structures & Algorithms/anagram-groups/submission-18.py
@@ -0,0 +1,16 @@
+from collections import defaultdict
+
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        count_to_sublist = defaultdict(list)
+
+        for s in strs:
+            count = [0] * 26
+
+            for c in s:
+                index = ord(c) - ord('a')
+                count[index] += 1
+            
+            count_to_sublist[tuple(count)].append(s)
+        
+        return list(count_to_sublist.values())
