4b9f3bcc7f47c09d2a6e4ca52fb6b70b728253f2
Drayiss <139669637+Drayiss@users.noreply.github.com>
2026-09-26 11:04:34 -0400

    Add: count-number-of-islands - submission-1

---
+++ Data Structures & Algorithms/count-number-of-islands/submission-1.py
@@ -0,0 +1,39 @@
+from collections import deque
+
+class Solution:
+    def numIslands(self, grid: List[List[str]]) -> int:
+        total_rows = len(grid)
+        total_cols = len(grid[0])
+
+        seen = set()
+
+        res = 0
+
+        def bfs(cell):
+            # Mark seen on enqueue
+            q = deque([cell])
+            seen.add(cell)
+
+            while q:
+                row, col = q.popleft()
+                
+                for dr, dc in [(0, 1), (1, 0), (0, -1), (-1, 0)]:
+                    nr, nc = dr + row, dc + col
+                    if (not 0 <= nr < total_rows
+                        or not 0 <= nc < total_cols
+                        or grid[nr][nc] == '0'
+                        or (nr, nc) in seen):
+                        continue
+                    
+                    seen.add((nr, nc))
+                    q.append((nr, nc))
+                    
+
+        for row in range(total_rows):
+            for col in range(total_cols):
+                if grid[row][col] == '1' and (row, col) not in seen:
+                    bfs((row, col))
+                    res += 1
+
+        return res
+        
